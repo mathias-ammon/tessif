e8bc7b62d0e231c5bf03c5aa2eff2286e8bef36d
tZE <WriteMe@NoEmail.net>
2023-02-14 16:24:50 +0100

    Upgrade Tessif's system model.

--- src/tessif/system_model.py
+++ src/tessif/system_model.py
@@ -8,13 +8,16 @@ import pickle
 import subprocess
 import tempfile
 
+import networkx as nx
+
 # third pary packages
 import pandas as pd
 
 # local packages
 import tessif.components as tessif_components
-from tessif.frused.defaults import registered_plugins
 import tessif.frused.namedtuples as nts
+from tessif import nxgraph
+from tessif.frused.defaults import registered_plugins
 from tessif.frused.paths import tessif_dir
 
 
@@ -94,20 +97,22 @@ class AbstractEnergySystem:
 
             # initialize instance respective instance attribute:
             if kwarg == "global_constraints":
-                setattr(self, "_{}".format(kwarg), kwargs_and_defaults[kwarg])
+                setattr(self, f"_{kwarg}", kwargs_and_defaults[kwarg])
 
             elif kwarg != "timeframe":
-                setattr(self, "_{}".format(kwarg), tuple(kwargs_and_defaults[kwarg]))
+                setattr(self, f"_{kwarg}", tuple(kwargs_and_defaults[kwarg]))
             else:
                 self._timeframe = kwargs_and_defaults[kwarg]
 
         self._es_attributes = tuple(kwargs_and_defaults.keys())
 
     def __repr__(self):
-        return "{!s}({!r})".format(self.__class__, self.__dict__)
+        """Verbosify representation."""
+        return f"{self.__class__!s}({self.__dict__!r})"
 
     def __str__(self):
-        return "{!s}(\n".format(self.__class__) + ",\n".join(
+        """Verbosify string representation."""
+        return f"{self.__class__!s}(\n" + ",\n".join(
             [
                 *[
                     "    {!r}={!r}".format(k.lstrip("_"), v)
@@ -119,82 +124,86 @@ class AbstractEnergySystem:
 
     @property
     def uid(self):
-        """:class:`~collections.abc.Hashable` unique identifier. Usually a
-        string aka a name.
+        """:class:`~collections.abc.Hashable` unique identifier.
+
+        Usually a string aka a name.
         """
         return self._uid
 
     @property
     def busses(self):
-        """:class:`~collections.abc.Generator` of
+        """Generator of the system model's busses.
+
+        :class:`~collections.abc.Generator` of
         :class:`~tessif.model.components.Bus` objects part
         of the energy system.
         """
-        for bus in self._busses:
-            yield bus
+        yield from self._busses
 
     @property
     def chps(self):
-        """:class:`~collections.abc.Generator` of
+        """Generator of the system model's CHPs.
+
+        :class:`~collections.abc.Generator` of
         :class:`~tessif.model.components.CHP`
         objects part of the energy system.
         """
-        for chp in self._chps:
-            yield chp
+        yield from self._chps
 
     @property
     def connectors(self):
-        """:class:`~collections.abc.Generator` of
+        """Generator of the system model's connectors.
+
+        :class:`~collections.abc.Generator` of
         :class:`~tessif.model.components.Connectors` objects part
         of the energy system.
         """
 
-        for connector in self._connectors:
-            yield connector
+        yield from self._connectors
 
     @property
     def sources(self):
-        """:class:`~collections.abc.Generator` of
+        """Generator of the system model's sources.
+
+        :class:`~collections.abc.Generator` of
         :class:`~tessif.model.components.Source` objects
         part of the energy system.
         """
-        for source in self._sources:
-            yield source
+        yield from self._sources
 
     @property
     def sinks(self):
-        """:class:`~collections.abc.Generator` of
+        """Generator of the system model's sinks.
+
+        :class:`~collections.abc.Generator` of
         :class:`~tessif.model.components.Sink` objects part
         of the energy system.
         """
-        for sink in self._sinks:
-            yield sink
+        yield from self._sinks
 
     @property
     def transformers(self):
-        """:class:`~collections.abc.Generator` of
+        """Generator of the system model's transformers.
+
+        :class:`~collections.abc.Generator` of
         :class:`~tessif.model.components.Transformer`
         objects part of the energy system.
         """
-        for transformer in self._transformers:
-            yield transformer
+        yield from self._transformers
 
     @property
     def storages(self):
-        """:class:`~collections.abc.Generator` of
+        """Generator of the system model's storages.
+
+        :class:`~collections.abc.Generator` of
         :class:`~tessif.model.components.Storage` objects
         part of the energy system.
         """
-        for storage in self._storages:
-            yield storage
+        yield from self._storages
 
     @property
     def nodes(self):
-        """
-        :class:`~collections.abc.Generator` yielding this energy system's
-        components.
-        """
-
+        """Generator yielding this system models' components."""
         component_types = [
             "busses",
             "chps",
@@ -206,12 +215,13 @@ class AbstractEnergySystem:
         ]
 
         for component_type in component_types:
-            for component in getattr(self, component_type):
-                yield component
+            yield from getattr(self, component_type)
 
     @property
     def edges(self):
-        """:class:`~collections.abc.Generator` of
+        """Generator yielding this system model's Graph representation edges.
+
+        :class:`~collections.abc.Generator` of
         :class:`~tessif.frused.namedtuples.Edge`
         :class:`NamedTuples<typing.Namedtuple>` representing graph like `edges
         <https://en.wikipedia.org/wiki/Glossary_of_graph_theory_terms#edge>`_.
@@ -248,7 +258,8 @@ class AbstractEnergySystem:
 
     @property
     def global_constraints(self):
-        """
+        """The system model's global constraints.
+
         :class:`Dictionary <dict>` of :class:`numeric <numbers.Number>`
         values mapped to global constraint naming :class:`strings <str>`
         currently respected by the energy system.
@@ -256,9 +267,7 @@ class AbstractEnergySystem:
         return self._global_constraints
 
     def _edge_carriers(self):
-        """
-        Extract carrier information out of busses and connectors.
-        """
+        """Extract carrier information out of busses and connectors."""
         _ecarriers = {}
         # All edge information are stored inside the bus objects
         for bus in self.busses:
@@ -336,7 +345,6 @@ class AbstractEnergySystem:
             The energy system created by connecting the
             :paramref:`~connect.energy_system` to this energy system.
         """
-
         components_to_add = list()
 
         for component in energy_system.nodes:
@@ -358,15 +366,15 @@ class AbstractEnergySystem:
             uid=self.uid,
             timeframe=self.timeframe,
             global_constraints=self.global_constraints,
-            components=set([*self.nodes, *components_to_add]),
+            components={*self.nodes, *components_to_add},
         )
 
         return connected_es
 
     def duplicate(self, prefix="", separator="_", suffix="copy"):
-        """
-        Duplicate the energy system and return it. Potentially modify
-        the node names.
+        """Duplicate the energy system and return it.
+
+        Potentially modify the node names.
 
         Parameters
         ----------
@@ -400,6 +408,7 @@ class AbstractEnergySystem:
 
     @property
     def timeframe(self):
+        """Timeframe representing the optimization time span."""
         return self._timeframe
 
     def pickle(self, location):
@@ -419,9 +428,11 @@ class AbstractEnergySystem:
 
     def tropp(self, plugin):
         """Transform, Optimize and PostProcess this Tessif system model."""
-
+        # Sanitize registered plugin
         rgstrd_plgn = registered_plugins[plugin]
 
+        # use a temporary directory to pickle and unpickle system models
+        # and results
         with tempfile.TemporaryDirectory() as tempdir:
 
             system_model_location = os.path.join(tempdir, "tessif_system_model.tsf")
@@ -454,7 +465,7 @@ class AbstractEnergySystem:
         uid,
         components,
         timeframe,
-        global_constraints={"emissions": float("+inf")},
+        global_constraints=None,
         **kwargs,
     ):
         """
@@ -504,6 +515,9 @@ class AbstractEnergySystem:
             The newly constructed energy system containing each component
             found in :paramref:`~from_components.components`.
         """
+        if not global_constraints:
+            global_constraints = {"emissions": float("+inf")}
+
         busses, chps, sinks, sources, transformers = [], [], [], [], []
         connectors, storages = [], []
 
@@ -538,22 +552,23 @@ class AbstractEnergySystem:
 
         return es
 
-    # def to_nxgrph(self):
-    #     """
-    #     Transform the :class:`AbstractEnergySystem` object into a
-    #     :class:`networkx.DiGraph` object.
+    def to_nxgrph(self):
+        """Transform tessif system model into networkx graph.
 
-    #     Return
-    #     ------
-    #     directional_graph: networkx.DiGraph
-    #         Networkx Directional Graph representing the abstract energy system.
-    #     """
-    #     grph = nx.DiGraph(name=self._uid)
-    #     for node in self.nodes:
-    #         grph.add_node(
-    #             str(node.uid),
-    #             **node.attributes,
-    #         )
+        Transform the :class:`AbstractEnergySystem` object into a
+        :class:`networkx.DiGraph` object.
 
-    #     nxgrph.create_edges(grph, self.edges, carrier=self._edge_carriers())
-    #     return grph
+        Return
+        ------
+        directional_graph: networkx.DiGraph
+            Networkx Directional Graph representing the abstract energy system.
+        """
+        grph = nx.DiGraph(name=self._uid)
+        for node in self.nodes:
+            grph.add_node(
+                str(node.uid),
+                **node.attributes,
+            )
+
+        nxgraph.create_edges(grph, self.edges, carrier=self._edge_carriers())
+        return grph
